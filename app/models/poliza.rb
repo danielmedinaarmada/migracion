bbd52fdafd816872b4cd4490668a2edb0786f78c
Daniel Medina <caracas2@caracas2-OptiPlex-330>
2018-09-19 10:15:32 -0400

    validar que la fecha de migracion no este en el pasado

--- app/models/poliza.rb
+++ app/models/poliza.rb
@@ -4,4 +4,12 @@ class Poliza < ApplicationRecord
   validates :poliza_a_migrar, presence: { message: ": la póliza no puede estar en blanco."}
   validates :poliza_a_migrar, format: { with: /\A3[\d{6}]+\z/,
     message: ": la póliza debe comenzar por 3 y todos los dígitos son numéricos." }
+  validate :expiration_date_cannot_be_in_the_past
+
+  def expiration_date_cannot_be_in_the_past
+    if fecha_migracion < Date.today
+      errors.add(:fecha_migracion, ": la fecha a migrar no puede ser anterior a hoy.")
+    end
+  end   
 end
+
